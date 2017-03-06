8ec564decb487c569a6dec5164b331a29f6e1caf
Vince Vitale <vincedvitale@gmail.com>
2017-03-06 12:28:38 -0600

    Added code into MyData.java & DataModel

--- app/src/main/java/com/example/vincevitale/recyclerviewwork/DataModel.java
+++ app/src/main/java/com/example/vincevitale/recyclerviewwork/DataModel.java
@@ -1,5 +1,30 @@
 package com.example.vincevitale.recyclerviewwork;
 
 public class DataModel {
+    String name;
+    String version;
+    int id_;
+    int image;
 
+    public DataModel(String name, String version, int id_, int image){
+        this.name = name;
+        this.version = version;
+        this.id_ = id_;
+        this.image = image;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getVersion(){
+        return version;
+    }
+
+    public int getId(){
+        return id_;
+    }
+    public int getImage(){
+        return image;
+    }
 }
--- app/src/main/java/com/example/vincevitale/recyclerviewwork/MyData.java
+++ app/src/main/java/com/example/vincevitale/recyclerviewwork/MyData.java
@@ -1,5 +1,13 @@
 package com.example.vincevitale.recyclerviewwork;
 
 public class MyData {
+    static String[] nameArray = {"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread",
+            "Honeycomb", "IceCreamSandwich", "JellyBean", "Lollipop", "Marshmallow"};
 
+    static String[] versionArray = {"1.5", "1.6", "2.0 - 2.0", "2.2 - 2.2.3", "2.3 - 2.3.7",
+            "3.0 - 3.2.6", "4.0 - 4.0.4", "4.1 - 4.3.1", "4.4 - 4.4.4", "6.0 - 6.0.1"};
+
+    static Integer[] drawableArray = {};
+
+    static Integer[] id = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 }
