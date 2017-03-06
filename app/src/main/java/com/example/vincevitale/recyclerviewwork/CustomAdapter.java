e55fd7d6e3fba2022c53823390b6769725333bdd
Vince Vitale <vincedvitale@gmail.com>
2017-03-06 12:50:42 -0600

    Added code into MainActivity.java

--- app/src/main/java/com/example/vincevitale/recyclerviewwork/CustomAdapter.java
+++ app/src/main/java/com/example/vincevitale/recyclerviewwork/CustomAdapter.java
@@ -1,5 +1,34 @@
 package com.example.vincevitale.recyclerviewwork;
 
-public class CustomAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textViewName;
+        TextView textViewVersion;
+        ImageView imageViewIcon;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+            textViewVersion = (TextView) itemView.findViewById(R.id.textViewVersion);
+            imageViewIcon = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+
+    }
+
+    private ArrayList<DataModel> dataSet;
+
+    public CustomAdapter(ArrayList<DataModel> data){
+        dataSet = data;
+    }
 }
--- app/src/main/java/com/example/vincevitale/recyclerviewwork/MainActivity.java
+++ app/src/main/java/com/example/vincevitale/recyclerviewwork/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.vincevitale.recyclerviewwork;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<DataModel> data;
+    static View.OnClickListener myClickLinstener;
+    private static ArrayList<Integer> removedItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Slide 15
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+
+        return true;
     }
 }
